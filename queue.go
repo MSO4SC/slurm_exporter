c3d12162233c24d6bfc9c6e7b6c55175f0cb0c26
Javier Carnero <javier.carnero@atos.net>
2017-06-19 13:52:18 +0200

    Common file to use with future sinfo and sacct
    - Time zone added as a flag, probably not needed and removed in the
      future.

--- queue.go
+++ queue.go
@@ -19,12 +19,9 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
-	"io"
-	"strconv"
 	"strings"
 	"time"
 
-	"github.com/mso4sc/slurm_exporter/ssh"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/log"
 )
@@ -69,60 +66,20 @@ func squeueLineParser(line string) ([]string, error) {
 	return result, nil
 }
 
-const (
-	sBOOTFAIL    = iota
-	sCANCELLED   = iota
-	sCOMPLETED   = iota
-	sCONFIGURING = iota
-	sCOMPLETING  = iota
-	sFAILED      = iota
-	sNODEFAIL    = iota
-	sPENDING     = iota
-	sPREEMPTED   = iota
-	sREVOKED     = iota
-	sRUNNING     = iota
-	sSPECIALEXIT = iota
-	sSTOPPED     = iota
-	sSUSPENDED   = iota
-	sTIMEOUT     = iota
-)
-
-// StatusDict maps string status with its int values
-var StatusDict = map[string]float64{
-	"BOOT_FAIL":    sBOOTFAIL,
-	"CANCELLED":    sCANCELLED,
-	"COMPLETED":    sCOMPLETED,
-	"CONFIGURING":  sCONFIGURING,
-	"COMPLETING":   sCOMPLETING,
-	"FAILED":       sFAILED,
-	"NODE_FAIL":    sNODEFAIL,
-	"PENDING":      sPENDING,
-	"PREEMPTED":    sPREEMPTED,
-	"REVOKED":      sREVOKED,
-	"RUNNING":      sRUNNING,
-	"SPECIAL_EXIT": sSPECIALEXIT,
-	"STOPPED":      sSTOPPED,
-	"SUSPENDED":    sSUSPENDED,
-	"TIMEOUT":      sTIMEOUT,
-}
-
 // QueueCollector collects metrics from the Slurm queues
 type QueueCollector struct {
-	waitTime *prometheus.Desc
-	status   *prometheus.Desc
-	host     string
-	sshUser  string
-	sshPass  string
-	timeZone string
+	waitTime    *prometheus.Desc
+	status      *prometheus.Desc
+	slurmCommon SlurmCollector
 }
 
 // NewQueueCollector creates a new Slurm Queue collector
-func NewQueueCollector(sshHost, sshUser, sshPass, timeZone string) *QueueCollector {
+func NewQueueCollector(host, sshUser, sshPass, timeZone string) *QueueCollector {
 	return &QueueCollector{
 		waitTime: prometheus.NewDesc(
 			"job_wait_time",
 			"Time that the job waited, or is estimated to wait",
-			[]string{"jobid", "name", "username", "partition", "numcpus"},
+			[]string{"jobid", "name", "username", "partition", "numcpus", "state"},
 			nil,
 		),
 		status: prometheus.NewDesc(
@@ -131,10 +88,12 @@ func NewQueueCollector(sshHost, sshUser, sshPass, timeZone string) *QueueCollect
 			[]string{"jobid", "name", "username", "partition", "numcpus"},
 			nil,
 		),
-		host:     sshHost,
-		sshUser:  sshUser,
-		sshPass:  sshPass,
-		timeZone: timeZone,
+		slurmCommon: SlurmCollector{
+			host:     host,
+			sshUser:  sshUser,
+			sshPass:  sshPass,
+			timeZone: timeZone,
+		},
 	}
 }
 
@@ -153,7 +112,7 @@ func (qc *QueueCollector) Collect(ch chan<- prometheus.Metric) {
 	fmt.Println("Collecting metrics...")
 	var stdout, stderr bytes.Buffer
 	var collected uint
-	err := qc.executeSSHCommand(
+	err := qc.slurmCommon.executeSSHCommand(
 		queueCommand,
 		&stdout,
 		&stderr)
@@ -175,27 +134,10 @@ func (qc *QueueCollector) Collect(ch chan<- prometheus.Metric) {
 		}
 
 		// parse submittime
-		submittime, stErr := time.Parse(slurmLayout, fields[qSUBMITTIME]+qc.timeZone)
+		submittime, stErr := time.Parse(slurmLayout, fields[qSUBMITTIME]+qc.slurmCommon.timeZone)
 		if stErr != nil {
 			log.Warn(stErr.Error())
 			continue
-		}
-
-		// parse starttime and send wait time
-		if fields[qSTARTTIME] != nullStartTime {
-			starttime, sstErr := time.Parse(slurmLayout, fields[qSTARTTIME]+qc.timeZone)
-			if sstErr == nil {
-				waitTimestamp := starttime.Unix() - submittime.Unix()
-				ch <- prometheus.MustNewConstMetric(
-					qc.waitTime,
-					prometheus.GaugeValue,
-					float64(waitTimestamp),
-					fields[qJOBID], fields[qNAME], fields[qUSERNAME],
-					fields[qPARTITION], fields[qNUMCPUS],
-				)
-			} else {
-				log.Warn(sstErr.Error())
-			}
 		}
 
 		// parse and send job state
@@ -209,112 +151,26 @@ func (qc *QueueCollector) Collect(ch chan<- prometheus.Metric) {
 				fields[qPARTITION], fields[qNUMCPUS],
 			)
 			collected++
+
+			// parse starttime and send wait time
+			if fields[qSTARTTIME] != nullStartTime {
+				starttime, sstErr := time.Parse(slurmLayout, fields[qSTARTTIME]+qc.slurmCommon.timeZone)
+				if sstErr == nil {
+					waitTimestamp := starttime.Unix() - submittime.Unix()
+					ch <- prometheus.MustNewConstMetric(
+						qc.waitTime,
+						prometheus.GaugeValue,
+						float64(waitTimestamp),
+						fields[qJOBID], fields[qNAME], fields[qUSERNAME],
+						fields[qPARTITION], fields[qNUMCPUS], fields[qSTATE],
+					)
+				} else {
+					log.Warn(sstErr.Error())
+				}
+			}
 		} else {
 			log.Warnf("Couldn't parse job status: %s", fields[qSTATE])
 		}
 	}
 	fmt.Printf("...%d jobs collected.\n", collected)
-}
-
-func (qc *QueueCollector) executeSSHCommand(cmd string, stdout, stderr io.Writer) error {
-	sshClient := ssh.NewSSHClientByPassword(
-		qc.sshUser,
-		qc.sshPass,
-		qc.host,
-		22,
-	)
-
-	command := &ssh.SSHCommand{
-		Path: cmd,
-		// Env:    []string{"LC_DIR=/usr"},
-		Stdin:  nil,
-		Stdout: stdout,
-		Stderr: stderr,
-	}
-
-	return sshClient.RunCommand(command)
-}
-
-func parseSlurmTime(field string) (uint64, error) {
-	var days, hours, minutes, seconds uint64
-	var err error
-
-	toParse := field
-	haveDays := false
-
-	// get days
-	slice := strings.Split(toParse, "-")
-	if len(slice) == 1 {
-		toParse = slice[0]
-	} else if len(slice) == 2 {
-		days, err = strconv.ParseUint(slice[0], 10, 64)
-		if err != nil {
-			return 0, err
-		}
-		toParse = slice[1]
-		haveDays = true
-	} else {
-		err = errors.New("slurm time could not be parsed: " + field)
-	}
-
-	// get hours, minutes and seconds
-	slice = strings.Split(toParse, ":")
-	if len(slice) == 3 {
-		hours, err = strconv.ParseUint(slice[0], 10, 64)
-		if err == nil {
-			minutes, err = strconv.ParseUint(slice[1], 10, 64)
-			if err == nil {
-				seconds, err = strconv.ParseUint(slice[1], 10, 64)
-			}
-		}
-		if err != nil {
-			return 0, err
-		}
-	} else if len(slice) == 2 {
-		if haveDays {
-			hours, err = strconv.ParseUint(slice[0], 10, 64)
-			if err == nil {
-				minutes, err = strconv.ParseUint(slice[1], 10, 64)
-			}
-		} else {
-			minutes, err = strconv.ParseUint(slice[0], 10, 64)
-			if err == nil {
-				seconds, err = strconv.ParseUint(slice[1], 10, 64)
-			}
-		}
-		if err != nil {
-			return 0, err
-		}
-	} else if len(slice) == 1 {
-		if haveDays {
-			hours, err = strconv.ParseUint(slice[0], 10, 64)
-		} else {
-			minutes, err = strconv.ParseUint(slice[0], 10, 64)
-		}
-		if err != nil {
-			return 0, err
-		}
-	} else {
-		err = errors.New("slurm time could not be parsed: " + field)
-	}
-
-	return days*24*60*60 + hours*60*60 + minutes*60 + seconds, nil
-}
-
-// nextLineIterator returns a function that iterates
-// over an io.Reader object returning each line  parsed
-// in fields following the parser method passed as argument
-func nextLineIterator(buf io.Reader, parser func(string) ([]string, error)) func() ([]string, error) {
-	var buffer = buf.(*bytes.Buffer)
-	var parse = parser
-	return func() ([]string, error) {
-		// get next line in buffer
-		line, err := buffer.ReadString('\n')
-		if err != nil {
-			return nil, err
-		}
-
-		// parse the line and return
-		return parse(line)
-	}
 }
